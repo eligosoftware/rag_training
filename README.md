7ed1b9ecd7a623b76260bed0e1f05e942a9c46be
Ilgar Rasulov <gragli98@gmail.com>
2025-03-03 14:58:36 +0100

    first run on VM, too slow, no answer

---
+++ README.md
@@ -0,0 +1,21 @@
+I will try to implement RAG to get some information from archive pdf files using Ollama.
+
+Install
+
+install also Visual Studio Build Tools 2022
+	Desktopentwicklung mit C++
+		MSVC v143 x64/x86 BuildTools
+		Windows 11 SDK
+		C++ -CMAKE Tools for Windows
+		C++ ATL für die neuets v143 BuildTools
+
+pip3 install pypdf
+
+pip3 install -U langchain-community
+
+pip3 install chromadb
+pip3 install langchain
+
+
+Install Ollama for Windows
+	Pull Mistral model (4.1 Gb): ollama pull mistral 
--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-from langchain.document_loaders import DirectoryLoader
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OllamaEmbeddings
-from langchain.llms import Ollama
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain_ollama import OllamaLLM
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains import RetrievalQA
 
 # Load all documents from an archive folder
-loader = DirectoryLoader("./archive/", glob="**/*.txt")  # Can support PDFs, Word, etc.
+loader = DirectoryLoader(
+    "./archive", 
+    glob="**/*.pdf", 
+    loader_cls=PyPDFLoader
+)
 documents = loader.load()
 
 # Convert text into vector embeddings
-vectorstore = Chroma.from_documents(documents, embedding=OllamaEmbeddings())
+vectorstore = Chroma.from_documents(documents, embedding=OllamaEmbeddings(model="mistral"))
 
 # Initialize Ollama model
-llm = Ollama(model="mistral")
+llm = OllamaLLM(model="mistral")
 
 # Create Retrieval-Based QA system
 qa = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriever())
 
 # Ask a question
-response = qa.run("What are the key points from last month's reports?")
+response = qa.run("What had Oliver found behind the hidden door?")
 print(response)
